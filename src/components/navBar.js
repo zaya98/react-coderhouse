9e156a80a4c9bc8b30a52439036b6d081f088b66
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-12-27 20:29:59 -0300

    primera pre-entrega

---
+++ src/components/navBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NavBar.css'
+
+function HeaderNav() {
+  return (
+    <div >
+        <ul className='navbar'>
+            <li> <a href='/'> inicio </a>  </li>
+            <li> <a href=''> contacto </a> </li>
+            <li> <a href=''> img </a> </li>
+        </ul>
+       
+    </div>
+  )
+  
+}
+
+export default HeaderNav
