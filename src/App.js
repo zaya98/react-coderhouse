dad582fb117fbe00366322c0cd14db5b1979ee7c
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-12-27 20:35:03 -0300

    primera pre-entrega

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeaderNav from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderNav />
+    </div>
+  );
+}
+
+export default App;
